6d08db2b30ecb760919c5a9dde154dd07a5eb461
mfbilgin <mf.bilgin0@hotmail.com>
2025-02-17 01:09:52 +0300

    Complete W1Q3

---
+++ src/Week_1/TrafficTicket.java
@@ -0,0 +1,23 @@
+package Week_1;
+
+import java.util.Scanner;
+
+public class TrafficTicket {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int speedLimit = 90;
+
+        System.out.print("Enter the speed limit: ");
+        int speed = scanner.nextInt();
+
+        if (speed > speedLimit && speed <= speedLimit + 20) {
+            System.out.println("You have exceeded the speed limit by 1-20 km/h. Your fine is ₺1000.");
+        } else if (speed > speedLimit + 20 && speed <= speedLimit + 40) {
+            System.out.println("You have exceeded the speed limit by 21-40 km/h. Your fine is ₺2000.");
+        } else if (speed > speedLimit + 40) {
+            System.out.println("You have exceeded the speed limit by more than 40 km/h. Your license is revoked.");
+        } else {
+            System.out.println("You are within the speed limit.");
+        }
+    }
+}
